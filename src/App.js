d88cc5955e717d50ed6b2f42d509aade03d69394
Muhammad Usman <prousman741@gmail.com>
2021-10-20 12:46:44 +0500

    files removed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p>This is the react party component</p>
+      <p>React-Party</p>
+    </div>
+  )
+}
+
+export default App
